f4f99da4d6ef83eafde4966575ce2307407af3cf
magixsource <magixsource.cn@gmail.com>
2015-04-09 16:12:15 +0800

    daterange初始框架

--- js/app/sinobest.daterange.js
+++ js/app/sinobest.daterange.js
@@ -3,6 +3,10 @@
         className:"sinobest-daterange", //CSS类名
         required:false, // 是否必录
         readonly:false,
+        beginMinDate:null,
+        beginMaxDate:null,
+        endMinDate:null,
+        endMaxDate:null,
         value:""
     };
 
@@ -26,14 +30,41 @@
         $daterange.display = function () {
         };
         $daterange.destory = function () {
+            $daterange.remove();
         };
 
         function render() {
+            var beginId;
+            $daterange.each(function (idx) {
+                if (idx == 0) {
+                    var beginSettings = settings;
 
+                    if (settings.beginMinDate) {
+                        beginSettings = $.extend({}, beginSettings, {minDate:settings.beginMinDate});
+                    }
+                    if (settings.beginMaxDate) {
+                        beginSettings = $.extend({}, beginSettings, {maxDate:settings.beginMaxDate});
+                    }
+                    $(this).sbdate(beginSettings);
+                    beginId = $(this).attr('id');
+                } else {
+                    var endSettings = settings;
+                    if (settings.endMinDate) {
+                        endSettings = $.extend({}, endSettings, {minDate:settings.endMinDate});
+                    } else {
+                        endSettings = $.extend({}, endSettings, {minDate:"#F{$dp.$D(\'" + beginId + "\')}"});
+                    }
+                    if (settings.endMaxDate) {
+                        endSettings = $.extend({}, endSettings, {maxDate:settings.endMaxDate});
+                    }
+                    $(this).sbdate(endSettings);
+                }
+            });
         }
 
-        return this.each(function () {
-            render();
-        });
+        //return this.each(function () {
+        render();
+        //});
+        return this;
     };
 })(jQuery);
