0bfd29503b12a78c33c961326924900fbecfdd2f
magixsource <magixsource.cn@gmail.com>
2015-04-09 15:07:24 +0800

    add date

--- js/app/sinobest.date.js
+++ js/app/sinobest.date.js
@@ -3,6 +3,17 @@
         className:"sinobest-date", //CSS类名
         required:false, // 是否必录
         readonly:false,
+        skin:'whyGreen',
+        isShowWeek:false,
+        isShowClear:true,
+        firstDayOfWeek:1,
+        dateFormat:"yyyy-MM-dd",
+        minDate:null,
+        maxDate:null,
+        onpicked:null,
+        onpicking:null,
+        onclearing:null,
+        oncleared:null,
         value:""
     };
 
@@ -12,24 +23,82 @@
         $date.settings = settings;
 
         $date.getValue = function () {
+            return $date.val();
         };
-        $date.setValue = function () {
+        $date.setValue = function (v) {
+            return  $date.val(v);
         };
         $date.getState = function () {
+            return $.extend({}, getAttributes());
         };
-        $date.setState = function () {
+        $date.setState = function (stateJson) {
+            $.each(stateJson, function (k, v) {
+                $date.attr(k, v);
+            });
+            return $date;
         };
         $date.getDom = function () {
+            return $date[0];
         };
         $date.reload = function () {
+            render();
         };
-        $date.display = function () {
+        $date.display = function (b) {
+            if (b) {
+                $date.show();
+            } else {
+                $date.hide();
+            }
         };
         $date.destory = function () {
+            $date.remove();
         };
 
         function render() {
+            $date.addClass(settings.className).addClass('Wdate');
+            if (settings.readonly) {
+                $date.attr('readonly', settings.readonly);
+            }
+            if (settings.required) {
+                $date.attr('readonly', settings.required);
+            }
+            if (settings.value) {
+                $date.val(settings.value);
+            }
+            $date.on('click', function () {
+                var initJson = {
+                    skin:settings.skin,
+                    firstDayOfWeek:settings.firstDayOfWeek,
+                    isShowClear:settings.isShowClear,
+                    isShowWeek:settings.isShowWeek,
+                    dateFmt:settings.dateFormat,
+                    onpicked:settings.onpicked,
+                    onpicking:settings.onpicking,
+                    onclearing:settings.onclearing,
+                    oncleared:settings.oncleared
+                };
+                if (settings.maxDate) {
+                    initJson = $.extend({}, initJson, {maxDate:settings.maxDate});
+                }
+                if (settings.minDate) {
+                    initJson = $.extend({}, initJson, {minDate:settings.minDate});
+                }
 
+                WdatePicker(initJson);
+            });
+        }
+
+        function getAttributes() {
+            var attributes = "{";
+            // DOM attributes
+            $.each($date[0].attributes, function (i, attr) {
+                if (i > 0) {
+                    attributes += ",";
+                }
+                attributes += ('"' + attr.name + '":"' + attr.value + '"');
+            });
+            attributes += "}";
+            return $.parseJSON(attributes);
         }
 
         return this.each(function () {
