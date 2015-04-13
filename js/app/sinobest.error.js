3045a55f5f219ddd3ce25f0bef0d299891baa925
magixsource <magixsource.cn@gmail.com>
2015-04-13 14:21:45 +0800

    移除sinobest.error

--- js/app/sinobest.error.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    var defaults = {
-    };
-    $.fn.sberror = function (options) {
-        var settings = $.extend({}, defaults, options || {});
-        var _this = this;
-    };
-})(jQuery);
