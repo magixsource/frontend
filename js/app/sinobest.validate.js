944fc45db3ed5475a4457956f18f4be205b7b7d5
magixsource <magixsource.cn@gmail.com>
2015-04-16 17:16:58 +0800

    validate改为在sbvalidator命名空间下

--- js/app/sinobest.validate.js
+++ js/app/sinobest.validate.js
@@ -1,54 +1,39 @@
 (function ($) {
-    $.extend({
-        sbvalidate:function (options) {
-//            var defaults = {};
-//            var settings = $.extend({}, defaults, options || {});
-
-            this.required = function (element, value) {
-                if (element.nodeName.toLowerCase() === "select") {
-                    // could be an array for select-multiple or a string, both are fine this way
-                    var val = $(element).val();
-                    return val && val.length > 0;
-                }
-                if (this.checkable(element)) {
-                    return this.getLength(value, element) > 0;
-                }
-                return $.trim(value).length > 0;
-            };
-            this.minlength = function (element, value, param) {
-                var length = $.isArray(value) ? value.length : this.getLength(value, element);
-                return length >= param;
+    $.extend({sbvalidator:{ }});
+    $.extend($.sbvalidator, {
+        required:function (element, value) {
+            if (element.nodeName.toLowerCase() === "select") {
+                // could be an array for select-multiple or a string, both are fine this way
+                var val = $(element).val();
+                return val && val.length > 0;
             }
-            this.maxlength = function (element, value, param) {
-                var length = $.isArray(value) ? value.length : this.getLength(value, element);
-                return length < param;
+            if (this.checkable(element)) {
+                return this.getLength(value, element) > 0;
             }
-
-            this.valid = function (regex, value) {
-                return regex.test(value);
-            };
-
-            this.checkable = function (element) {
-                return ( /radio|checkbox/i ).test(element.type);
-            };
-
-
-            this.getLength = function (value, element) {
-                switch (element.nodeName.toLowerCase()) {
-                    case "select":
-                        return $("option:selected", element).length;
-                    case "input":
-                        if (this.checkable(element)) {
-                            return this.findByName(element.name).filter(":checked").length;
-                        }
-                }
-                return value.length;
-            };
-            this.findByName = function (name) {
-                return $("input[name='" + name + "']");
-            };
-
-            return this;
+            return $.trim(value).length > 0;
+        },
+        checkable:function (element) {
+            return ( /radio|checkbox/i ).test(element.type);
+        }, getLength:function (value, element) {
+            switch (element.nodeName.toLowerCase()) {
+                case "select":
+                    return $("option:selected", element).length;
+                case "input":
+                    if (this.checkable(element)) {
+                        return this.findByName(element.name).filter(":checked").length;
+                    }
+            }
+            return value.length;
+        }, findByName:function (name) {
+            return $("input[name='" + name + "']");
+        }, minlength:function (element, value, param) {
+            var length = $.isArray(value) ? value.length : this.getLength(value, element);
+            return length >= param;
+        }, maxlength:function (element, value, param) {
+            var length = $.isArray(value) ? value.length : this.getLength(value, element);
+            return length < param;
+        }, valid:function (regex, value) {
+            return regex.test(value);
         }
 
     });
