a92805a092a3a137b94cae7f70dc6e4d096e7e4b
magixsource <magixsource.cn@gmail.com>
2015-04-23 15:17:39 +0800

    ecma5增加string.format

--- js/app/sinobest.ecma5.js
+++ js/app/sinobest.ecma5.js
@@ -1,12 +1,23 @@
-Array.prototype.indexOf = function(e){
-  for(var i=0,j; j=this[i]; i++){
-    if(j==e){return i;}
-  }
-  return -1;
+Array.prototype.indexOf = function (e) {
+    for (var i = 0, j; j = this[i]; i++) {
+        if (j == e) {
+            return i;
+        }
+    }
+    return -1;
 }
-Array.prototype.lastIndexOf = function(e){
-  for(var i=this.length-1,j; j=this[i]; i--){
-    if(j==e){return i;}
-  }
-  return -1;
+Array.prototype.lastIndexOf = function (e) {
+    for (var i = this.length - 1, j; j = this[i]; i--) {
+        if (j == e) {
+            return i;
+        }
+    }
+    return -1;
+}
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/\{(\d+)\}/g,
+        function (m, i) {
+            return args[i];
+        });
 }
