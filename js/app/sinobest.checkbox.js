39e4a5a3fa9364a717e8e769dc48bdc561b57d4a
magixsource <magixsource.cn@gmail.com>
2015-04-23 15:21:39 +0800

    Checkbox异常消息重构

--- js/app/sinobest.checkbox.js
+++ js/app/sinobest.checkbox.js
@@ -222,10 +222,10 @@
                     },
                     error:function (XMLHttpRequest, textStatus, errorThrown) {
                         var e = new Object();
-                        e.code = textStatus;
-                        e.msg = "无法请求获取数据，请求地址：" + this.url;
+                        e.code = XMLHttpRequest.status;
+                        e.msg = $.sberror.format(e.code, this.url);
                         // 根据规范要求将错误交给全局函数处理
-                        $.fn.sberror.onerror(e);
+                        $.sberror.onError(e);
                     }
                 });
             } else {
