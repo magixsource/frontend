75447af412a4d2689f14e9e031f3b87af5ada18e
magixsource <magixsource.cn@gmail.com>
2015-05-11 10:32:41 +0800

    [M]为控件增加onClick，支持窗口关闭时回调

--- js/app/sinobest.dialog.js
+++ js/app/sinobest.dialog.js
@@ -1,23 +1,24 @@
 (function ($) {
     $.extend({
-        sbdialog:function (options) {
+        sbdialog: function (options) {
             var defaults = {
-                containerId:null,
-                content:null, // 设置content之后，会覆盖container的内容
-                url:null, // 页面地址
-                className:"sinobest-dialog", //CSS类名
-                dialogType:"dialog",
-                text:"",
-                width:null,
-                height:null,
-                zIndex:9999, //影响全局
-                title:null,
-                okValue:"确定",
-                cancelValue:"取消",
-                onOK:function () {
+                containerId: null,
+                content: null, // 设置content之后，会覆盖container的内容
+                url: null, // 页面地址
+                className: "sinobest-dialog", //CSS类名
+                dialogType: "dialog",
+                text: "",
+                width: null,
+                height: null,
+                zIndex: 9999, //影响全局
+                title: null,
+                okValue: "确定",
+                cancelValue: "取消",
+                onOK: function () {
                 },
-                onCancel:null,
-                button:null// 自定义按钮
+                onCancel: null,
+                onClose: null,
+                button: null// 自定义按钮
             };
             var settings = $.extend({}, defaults, options || {});
 
@@ -100,34 +101,37 @@
                 if (settings.dialogType == 'alert') {
                     //$.dialog({content:settings.text});
                     instance = basalDialog({
-                        content:settings.text,
-                        zIndex:settings.zIndex,
-                        width:settings.width,
-                        height:settings.height,
-                        cancel:false,
-                        okValue:settings.okValue,
-                        ok:settings.onOK
+                        content: settings.text,
+                        zIndex: settings.zIndex,
+                        width: settings.width,
+                        height: settings.height,
+                        cancel: false,
+                        okValue: settings.okValue,
+                        ok: settings.onOK,
+                        onclose: settings.onClose
                     });
                     $(instance.node).addClass(settings.className);
                     instance.show();
                 } else if (settings.dialogType == 'confirm') {
                     instance = basalDialog({
-                        content:settings.text,
-                        zIndex:settings.zIndex,
-                        width:settings.width,
-                        height:settings.height,
-                        okValue:settings.okValue,
-                        ok:settings.onOK,
-                        cancelValue:settings.cancelValue,
-                        cancel:settings.onCancel
+                        content: settings.text,
+                        zIndex: settings.zIndex,
+                        width: settings.width,
+                        height: settings.height,
+                        okValue: settings.okValue,
+                        ok: settings.onOK,
+                        cancelValue: settings.cancelValue,
+                        cancel: settings.onCancel,
+                        onclose: settings.onClose
                     });
                     instance.show();
                 } else if (settings.dialogType == 'message') {
                     instance = basalDialog({
-                        zIndex:settings.zIndex,
-                        content:settings.text,
-                        width:settings.width,
-                        height:settings.height
+                        zIndex: settings.zIndex,
+                        content: settings.text,
+                        width: settings.width,
+                        height: settings.height,
+                        onclose: settings.onClose
                     });
                     $(instance.node).addClass(settings.className);
                     instance.show();
@@ -139,15 +143,16 @@
 
                 } else if (settings.dialogType == 'modal') {
                     var json = {
-                        title:settings.title,
-                        zIndex:settings.zIndex,
-                        width:settings.width,
-                        height:settings.height,
-                        button:settings.button
+                        title: settings.title,
+                        zIndex: settings.zIndex,
+                        width: settings.width,
+                        height: settings.height,
+                        button: settings.button,
+                        onclose: settings.onClose
                     };
                     // 判断是否iframe弹出
                     if (settings.url) {
-                        json = $.extend({}, json, {url:settings.url});
+                        json = $.extend({}, json, {url: settings.url});
                     } else {
                         var content = null;
                         // 是否自定义弹出内容
@@ -157,7 +162,7 @@
                             // DOM容器
                             content = $("#" + settings.containerId).html();
                         }
-                        json = $.extend({}, json, {content:content});
+                        json = $.extend({}, json, {content: content});
                     }
 
                     instance = basalDialog(json);
@@ -166,14 +171,15 @@
                 } else {
                     // handle as dialog
                     var json = {
-                        title:settings.title,
-                        zIndex:settings.zIndex,
-                        width:settings.width,
-                        height:settings.height,
-                        button:settings.button
+                        title: settings.title,
+                        zIndex: settings.zIndex,
+                        width: settings.width,
+                        height: settings.height,
+                        button: settings.button,
+                        onclose: settings.onClose
                     };
                     if (settings.url) {
-                        json = $.extend({}, json, {url:settings.url});
+                        json = $.extend({}, json, {url: settings.url});
                     } else {
                         var content = null;
                         if (settings.content) {
@@ -181,7 +187,7 @@
                         } else {
                             content = $("#" + settings.containerId).html();
                         }
-                        json = $.extend({}, json, {content:content});
+                        json = $.extend({}, json, {content: content});
                     }
 
                     instance = basalDialog(json);
