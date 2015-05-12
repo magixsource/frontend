cf32e72b6b96579358095d08a1542abd1865121d
magixsource <magixsource.cn@gmail.com>
2015-05-12 09:12:59 +0800

    [A] loading status is complete

--- js/app/sinobest.loadingstatus.js
+++ js/app/sinobest.loadingstatus.js
@@ -1,11 +1,71 @@
+/**
+ * Loading status is a component that using in ajax invoked
+ * to show a loading img mean ajax is requesting,please wait
+ */
 (function ($) {
-    $.extend({sbloadingstatus: {}});
-    $.extend($.sbloadingstatus, {
-        open: function () {
+    $.extend({
+        sbloadingstatus: function (options) {
+            var defaults = {
+                className: "sinobest-loadingstatus", //CSS类名
+                loadingClassName: "sinobest-loading",
+                mode: "auto"
+            };
+            var settings = $.extend({}, defaults, options || {});
+            var $this = this;
 
-        },
-        close: function () {
+            /**
+             * Add loading class to body
+             */
+            this.open = function () {
+                $('body').addClass(settings.loadingClassName);
+                // ajax
+                $(document).on({
+                    ajaxStop: function () {
+                        $this.close();
+                    }
+                });
+            };
 
+            /**
+             * Remove loading class from body
+             */
+            this.close = function () {
+                $('body').removeClass(settings.loadingClassName);
+            };
+
+            /**
+             * Render component
+             */
+            function render() {
+                buildContainer();
+                addListener();
+            };
+
+            /**
+             * Loading status component need a container,DOM span here
+             */
+            function buildContainer() {
+                $this.$container = '<span class="' + settings.className + '"></span>';
+                $("body").append($this.$container);
+            };
+            /**
+             * Usually,we use auto mode just
+             */
+            function addListener() {
+                if (settings.mode == 'auto') {
+                    $(document).on({
+                        ajaxStart: function () {
+                            $('body').addClass(settings.loadingClassName);
+                        },
+                        ajaxStop: function () {
+                            $this.close();
+                        }
+                    });
+                }
+            };
+
+            render();
+            return this;
         }
     });
 
